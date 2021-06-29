71fab3e7147e71a5d76232b1060a0cf996887b29
halilweis <49692282+halilweis@users.noreply.github.com>
2021-06-29 22:18:44 +0500

    Add files via upload

--- framework.sql
+++ framework.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Июн 27 2021 г., 15:13
+-- Время создания: Июн 29 2021 г., 19:18
 -- Версия сервера: 10.4.13-MariaDB
 -- Версия PHP: 7.3.20
 
@@ -33,27 +33,28 @@ CREATE TABLE `todo` (
   `userPassword` varchar(255) NOT NULL,
   `email` varchar(50) NOT NULL,
   `taskText` text NOT NULL,
-  `status` tinyint(1) NOT NULL
+  `status` tinyint(1) NOT NULL,
+  `isEdited` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `todo`
 --
 
-INSERT INTO `todo` (`id`, `userName`, `userPassword`, `email`, `taskText`, `status`) VALUES
-(1, 'ekgesh', '12345', 'ekgesh@aa.com', 'csmcoisjcojs', 0),
-(2, 'Ali', '', 'test@mail.com', 'Newtaskaaaa', 0),
-(3, 'Ali', '', 'test@mail.com', 'Newtask', 0),
-(4, 'dewdwd', '', 'wdee@cc.com', 'cdcdcdfd', 0),
-(5, 'dewdwd', '', 'wdee@cc.com', 'cdcdcdfd', 0),
-(6, 'dwedw', '', 'aaa@gmail.com', 'dwjdiwjdwijdwjdwidjid', 0),
-(7, 'asa', '', 'aa@aa.com', 'edwedew111', 1),
-(8, 'asa', '', 'aa@aa.com', 'edwedewdwew', 1),
-(9, 'asa', '', 'aa@aa.com', 'edwedew', 0),
-(10, 'sasa', '', 'aa@aa.com', 'dsdss', 0),
-(11, 'asa', '', 'asa@cc.com', 'dsdsd', 0),
-(12, 'yyv', '', 'rrdr@vv.com', 'vyvyv', 0),
-(13, 'hbhbj', '', 'hh@cc.com', 'ububuybubu', 0);
+INSERT INTO `todo` (`id`, `userName`, `userPassword`, `email`, `taskText`, `status`, `isEdited`) VALUES
+(1, 'ekgesh', '12345', 'ekgesh@aa.com', 'csmcoisjcojsddssd', 1, 1),
+(2, 'Ali', '', 'test@mail.com', 'Newtaskaaaahalil', 0, 1),
+(3, 'Ali', '', 'test@mail.com', 'Newtaskqqqqq', 1, 1),
+(4, 'dewdwd', '', 'wdee@cc.com', 'cdcdcdfd', 0, 0),
+(5, 'dewdwd', '', 'wdee@cc.com', 'cdcdcdfd', 0, 0),
+(6, 'dwedw', '', 'aaa@gmail.com', 'dwjdiwjdwijdwjdwidjid', 0, 0),
+(7, 'asa', '', 'aa@aa.com', 'edwedew111', 1, 0),
+(8, 'asa', '', 'aa@aa.com', 'edwedewdwew', 1, 0),
+(9, 'asa', '', 'aa@aa.com', 'edwedew', 0, 0),
+(10, 'sasa', '', 'aa@aa.com', 'dsdss', 0, 0),
+(11, 'asa', '', 'asa@cc.com', 'dsdsd', 0, 0),
+(12, 'yyv', '', 'rrdr@vv.com', 'vyvyv', 0, 0),
+(13, 'hbhbj', '', 'hh@cc.com', 'halil', 0, 1);
 
 --
 -- Индексы сохранённых таблиц
